11707b4ac6578b9a44f5556556c54512206685b6
Seva <seva.leonov@hotmail.com>
2024-07-12 22:06:52 +0700

    Add API for finding & returning users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,9 +2,28 @@ import asyncHandler from "express-async-handler";
 import User from "../models/user";
 
 const getUser = asyncHandler(async (req, res) => {
-  console.log(req);
-  console.log(res);
-  res.status(200).json({ success: true });
+  const { email, number } = req.query;
+
+  if (!email) {
+    res.status(400).json({ error: "Email is missing" });
+  }
+
+  // Find users that match both the email and the phone number
+  if (email && number) {
+    const users = await User.find({
+      email: email,
+      number: number,
+    });
+    res.status(200).json({ success: users });
+  }
+
+  // Find users that match the email
+  if (email && !number) {
+    const users = await User.find({
+      email: email,
+    });
+    res.status(200).json({ success: users });
+  }
 });
 
 export default {
